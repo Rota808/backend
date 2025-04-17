723e348b1f19b161815c966bbe670848d646082b
Niet <antonionetodev@yahoo.com>
2025-04-17 18:46:21 -0300

    wsgi

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH="/home/site/wwwroot/backend/back:$PYTHONPATH"
 cd /home/site/wwwroot/backend/back
-gunicorn --bind 0.0.0.0:8000 back.wsgi
+gunicorn --bind 0.0.0.0:8000 back.back.wsgi
