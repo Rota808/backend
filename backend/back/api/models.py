1bf5f79a3980f5ee22e6e09e53901578796e14a0
Niet <antonionetodev@yahoo.com>
2025-04-19 20:55:19 -0300

    corrige a definição do método de pagamento Mercado Pago

--- backend/back/api/models.py
+++ backend/back/api/models.py
@@ -72,7 +72,7 @@ class Payment(models.Model):
         ('debit_card', 'Debit Card'),
         ('cash', 'Cash'),
         ('pix', 'PIX'),
-        ('mercado_pago', 'Mercado Pago', 'mercadopago')
+        ('mercado_pago', 'mercadopago')
     ]
     
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
